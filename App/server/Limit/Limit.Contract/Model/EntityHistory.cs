d4b110ba2e097b65851ee91faae8f28d6bc9cbda
Dmitrii Milyaev <dmitriy.milyaev@outlook.com>
2024-06-23 16:20:01 +0500

    [Feature-0.0.1] Добавил модель Db::PersonHistory

---
+++ App/server/Limit/Limit.Contract/Model/EntityHistory.cs
@@ -0,0 +1,9 @@
+﻿namespace Limit.Contract.Model
+{
+    public class EntityHistory : Entity
+    {
+        public long HId { get; set; }
+        public DateTimeOffset ChangeDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ App/server/Limit/Limit.Db/Model/PersonHistory.cs
@@ -0,0 +1,27 @@
+﻿using Limit.Db.Attributes;
+using System;
+
+namespace Limit.Db.Model
+{
+    [TableName("h_person")]
+    public class PersonHistory : EntityHistory
+    {
+        [ColumnName("userid")]
+        public Guid UserId { get; set; }
+
+        [ColumnName("name")]
+        public string Name { get; set; }
+
+        [ColumnName("description")]
+        public string Description { get; set; }
+
+        [ColumnName("locationid")]
+        public Guid LocationId { get; set; }
+
+        [ColumnName("lastactivedate")]
+        public DateTimeOffset LastActiveDate { get; set; }
+
+        [ColumnName("createddate")]
+        public DateTimeOffset Createddate { get; set; }
+    }
+}
